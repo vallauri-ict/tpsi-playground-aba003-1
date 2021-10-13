aad8bf2b87a2b33c9862fad8ee5afba59b832eda
Ababei Edoardo <e.ababei.1257@vallauri.edu>
2021-10-13 09:36:53 +0200

    Esercizio 03 - Finished

--- Typescript/Esercizio 03- People/server.ts
+++ Typescript/Esercizio 03- People/server.ts
@@ -1,5 +1,6 @@
 import { prototype } from 'events';
 import *  as _http from 'http';
+import { json } from 'stream/consumers';
 let HEADERS = require("./headers.json");
 let dispatcher = require("./dispatcher.ts");
 import * as persons from './persons.json';
@@ -25,6 +26,70 @@ dispatcher.addListener("GET","/api/nazioni",function (req:any,res:any) {
    nazioni.sort();
    res.write(JSON.stringify({"nazioni":nazioni}));
     res.end();
+})
+dispatcher.addListener("GET","/api/persone",function (req:any,res:any) {   
+    res.writeHead(200,HEADERS.json);
+    let nazione = req["GET"].nazione; // i parametri sono passati e messi in questo campo nel dispatcher
+    let json =[];
+  for (const person of persons["results"]) {
+      if (person.location.country==nazione) {
+       let jsonPerson ={
+           "name":person.name.title +" " + person.name.first +" "+ person.name.last,
+           "city":person.location.city,
+           "state":person.location.state,
+           "cell":person.cell
+       }
+       json.push(jsonPerson);
+    }
+   
+  }
+   res.write(JSON.stringify(json));
+    res.end();
+})
+
+dispatcher.addListener("PATCH","/api/dettagli",function (req,res) {
+   
+    let persona=[];
+    let personReq =req.BODY.person;
+    
+    for (const person of persons.results) {
+        if (person.name.title +" " + person.name.first +" "+ person.name.last===personReq) {
+           persona.push(person);
+            break;
+        }
+    }
+    if (persona.length>0) {
+        res.writeHead(200,HEADERS.json);
+        res.write(JSON.stringify(persona));
+         res.end();
+    }else{
+        res.writeHead(404,HEADERS.text);
+        res.write("Persona non trovata");
+         res.end();
+    }     
+
+})
+dispatcher.addListener("DELETE","/api/elimina",function (req:any,res:any) {   
+    let trovata=false;
+    let personReq =req.BODY.person;
+    let i;
+    for (i =0;i < persons.results.length; i++) {
+        if (persons.results[i].name.title +" " + persons.results[i].name.first +" "+ persons.results[i].name.last===personReq) {
+           trovata=true;
+             break;
+         }
+        
+    }
+    if (trovata) {
+        persons.results.splice(i,1);
+        res.writeHead(200,HEADERS.json);
+        res.write(JSON.stringify("Record cancellato correttamente"));
+         res.end();
+    }else{
+        res.writeHead(404,HEADERS.text);
+        res.write("Errore persona non trovata");
+         res.end();
+    }     
 })
 
 
--- Typescript/Esercizio 03- People/static/index.js
+++ Typescript/Esercizio 03- People/static/index.js
@@ -1,19 +1,95 @@
-"use strict"
-$(document).ready(function() {
-    let _lstNazioni = $("#lstNazioni");
-    let _tabStudenti = $("#tabStudenti");
-    let _divDettagli = $("#divDettagli");
+"use strict";
 
-    _divDettagli.hide();
+$(document).ready(function () {
+  let _lstNazioni = $("#lstNazioni");
+  let _tabStudenti = $("#tabStudenti");
+  let _divDettagli = $("#divDettagli");
+  let selectedNation;
+  _divDettagli.hide();
 
-    let request=inviaRichiesta("get","/api/nazioni");
+  let request = inviaRichiesta("get", "/api/nazioni");
+  request.fail(errore);
+  request.done(function (data) {
+    request.done((data) => {
+      console.log(data);
+
+      for (const nation of data.nazioni) {
+        $("<a>", {
+          class: "dropdown-item",
+          href: "#",
+          text: nation,
+          click: visualizzaPersona,
+        }).appendTo(_lstNazioni);
+      }
+    });
+  });
+  function visualizzaPersona() {
+    //bottone dell listbox
+    if ($(this).text()) {
+      selectedNation = $(this).text();
+    }
+    let nation = $(this).text();
+    //i parametri li mandiamo ad invia richiesta sempre in json
+    let request = inviaRichiesta("get", "/api/persone", {
+      nazione: selectedNation,
+    });
     request.fail(errore);
-    request.done(function(data){
-        console.log(data);
-    })
+    request.done(function (data) {
+      _tabStudenti.empty();
+      _divDettagli.hide();
+      for (const person of data) {
+        let tr = $("<tr>").appendTo(_tabStudenti);
+        for (const key in person) {
+          $("<td>").appendTo(tr).text(person[key]);
+        }
 
- 
- 
- 
- 
-})
+        let td = $("<td>").appendTo(tr);
+        let button = $("<button>");
+        button.appendTo(td);
+        button.text("Dettagli");
+        button.click(dettagli);
+        button.prop("name", person.name);
+
+        td = $("<td>").appendTo(tr);
+        button = $("<button>");
+        button.appendTo(td);
+        button.text("Elimina");
+        button.click(elimina);
+        button.addClass("Eliminazione");
+        button.prop("name", person.name);
+      }
+    });
+  }
+
+  function dettagli() {
+    let name = $(this).prop("name");
+    let request = inviaRichiesta("PATCH", "/api/dettagli", {
+      person: $(this).prop("name"),
+    });
+    request.fail(errore);
+    request.done(function (data) {
+      console.log(data);
+      _divDettagli.show(1000);
+      _divDettagli
+        .children(".card-img-top")
+        .prop("src", data[0].picture.thumbnail);
+      _divDettagli.find(".card-title").text(name);
+      _divDettagli.find(".card-title").text(name);
+      let s = `<b>Gender: </b>${data[0].gender}<br>`;
+      s += `<b>Address: </b>${JSON.stringify(data[0].location)}<br>`;
+      s += `<b>Address: </b>${data[0].email}<br>`;
+      s += `<b>Address: </b>${JSON.stringify(data[0].dob)}<br>`;
+      _divDettagli.find(".card-text").text(s);
+    });
+  }
+  function elimina() {
+    let request = inviaRichiesta("DELETE", "/api/elimina", {
+      person: $(this).prop("name"),
+    });
+    request.fail(errore);
+    request.done(function (message) {
+      alert(message);
+      visualizzaPersona();
+    });
+  }
+});
