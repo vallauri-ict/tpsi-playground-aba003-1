f1c95a8924eb30b75a5f52bc103f99ff171ecd5f
Ababei Edoardo <e.ababei.1257@vallauri.edu>
2021-10-17 15:21:52 +0200

    Added Esercizio 05 - Radios to know: a bit slow at the beginning due to the amount of the requests of the list box val tutti

--- Typescript/Esercizio 05 - Radio/server.ts
+++ Typescript/Esercizio 05 - Radio/server.ts
@@ -4,31 +4,111 @@ import *  as _http from 'http';
 import { json } from 'stream/consumers';
 import { HEADERS } from "./headers";
 import { Dispatcher } from './dispatcher';
-import * as radios from './radios.json';
+import radios from './radios.json';
 import states from "./states.json";
 import * as _fs from 'fs';
 import * as _mime from 'mime';
+import { parse } from 'path/posix';
 
 let dispatcher = new Dispatcher();
-let PORT = 1338;
+let PORT = 1337;
 let server = _http.createServer(function (req, res) { //funzione richiamata ogni volta 
-    //se è statico cerca il file nel file sistem se no si deve cercare il servizio nel vettore associativo del disatcher
-    dispatcher.dispatch(req, res);
+  //se è statico cerca il file nel file sistem se no si deve cercare il servizio nel vettore associativo del disatcher
+  dispatcher.dispatch(req, res);
 })
 server.listen(PORT);
 console.log("Server in ascolto sulla porta: " + PORT);
 
 
 //********************registrazione dei servizi*****************************
+dispatcher.addListener("POST", "/api/like", function (req, res) {
+  let nome = req["BODY"].radio
 
+  for (const radio of radios) {
+    if (radio.name == nome) {
+      let num = parseInt(radio.votes)
+      num++;
+      radio.votes = num.toString();
+      _fs.writeFile("./radios.json", JSON.stringify(radios), function (err) {
+        if (err) {
+          res.writeHead(404, HEADERS.text);
+          res.write(err);
+        }
+        else {
+          res.writeHead(200, HEADERS.json);
+          res.write(JSON.stringify(radio.votes));
+        }
+        res.end();
+      })
+      break;
 
-dispatcher.addListener("GET", "/api/states",function(req,res){
-    let vetState = [];
-  for (const state of states) {
-    vetState.push(state.name + "-" + state.stationcount);
+    }
   }
-  vetState.sort();
-  res.writeHead(200, HEADERS.json);
-  res.write(JSON.stringify(vetState));
+
+})
+
+dispatcher.addListener("GET", "/api/radios", function (req, res) {
+  let state = req["GET"].state
+  let arrRadios = [];
+  if (state == 'tutti') {
+    for (const radio of radios) {
+      arrRadios.push(radio);
+    }
+  } else {
+    for (const radio of radios) {
+      if (radio.state == state) {
+        arrRadios.push(radio);
+      }
+    }
+
+  }
+  res.writeHead(200, HEADERS.json)
+  res.write(JSON.stringify(arrRadios));
   res.end();
+
+
+})
+
+dispatcher.addListener("GET", "/api/elenco", function (req, res) {
+  res.writeHead(200, HEADERS.json)
+  res.write(JSON.stringify(states));
+  res.end();
+
+})
+dispatcher.addListener("GET", "/api/update", function (req, res) {
+
+  zeroCount();
+  for (const radio of radios) {
+    for (const state of states) {
+      if (radio.state == state.name) {
+        let num = parseInt(state.stationcount)
+        num++;
+        state.stationcount = num.toString();
+      }
+    }
+  }
+  writeOnDisk();
+
+  function zeroCount() {
+    for (const state of states) {
+      let num = 0
+      state.stationcount = num.toString();
+    }
+    writeOnDisk();
+
+  }
+
+  function writeOnDisk() {
+    _fs.writeFile("./states.json", JSON.stringify(states), function (err) {
+      if (err) {
+        res.writeHead(404, HEADERS.text);
+        res.write(err);
+      }
+      else {
+        res.writeHead(200, HEADERS.json);
+        res.write(JSON.stringify("Json salvato correttamente su disco"));
+      }
+      res.end();
+    })
+  }
 })
--- Typescript/Esercizio 05 - Radio/static/index.js
+++ Typescript/Esercizio 05 - Radio/static/index.js
@@ -1,8 +1,74 @@
-"use strict"
+"use strict";
+
 $(document).ready(function () {
-   let request = inviaRichiesta("get", "/api/states")
-   request.fail(errore);
-   request.done(function (data) {
-       console.log(data)
-   })
-})
+  let lstRegioni = $("#lstRegioni");
+
+  let request = inviaRichiesta("get", "/api/update");
+  request.fail(errore);
+  request.done(function (data) {
+    console.log(data);
+  });
+
+  let request2 = inviaRichiesta("get", "/api/elenco");
+  request2.fail(errore);
+  request2.done(function (data) {
+    console.log(data);
+    for (const state of data) {
+      let option = $("<option>").appendTo(lstRegioni);
+      option.text(state.name + `[${state.stationcount} emittenti] `);
+    }
+    lstRegioni.val('tutti').change();
+  });
+
+  lstRegioni.on("change", function () {
+    
+    let tbody = $("#tbody");
+    tbody.empty();
+    let regione = lstRegioni.val().split("[")[0];
+    
+    let request3 = inviaRichiesta("GET", "/api/radios", { state: regione });
+    request3.fail(errore);
+    request3.done(function (radios) {
+      for (const radio of radios) {
+        let tr = $("<tr>").appendTo(tbody);
+        let td = $("<td>").appendTo(tr);
+        let img = $("<img>").appendTo(td);
+        img.prop("src", radio.favicon);
+        img.css({ width: "40px", height: "40px" });
+
+        td = $("<td>").appendTo(tr);
+        td.text(radio.name);
+
+        td = $("<td>").appendTo(tr);
+        td.text(radio.codec);
+
+        
+        td = $("<td>").appendTo(tr);
+        td.text(radio.bitrate);
+
+        
+        td = $("<td>").appendTo(tr);
+        td.text(radio.votes);
+
+         td = $("<td>").appendTo(tr);
+         img = $("<img>").appendTo(td);
+        img.prop("src", "./like.jpg");
+        img.css({ width: "40px", height: "40px" });
+        img.prop("radio",radio.name)
+        img.click(function () {
+            
+            let request4=  inviaRichiesta("POST", "/api/like", { radio: $(this).prop("radio") });
+            let _this = $(this);
+            request4.fail(errore);
+            request4.done(function (data) {
+                console.log(data);
+                _this.parent().prev().text(data)
+            })
+        })
+
+        
+
+      }
+    });
+  });
+});
